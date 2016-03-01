80b72449f948cf682d0ecdf1b86ce00f69ff3004
mohammad12344 <sdf12345641@gmail.com>
2016-03-01 21:30:22 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -212,24 +212,6 @@ function create_config( )
     "welcome",
     "addplugin",
     "translate",
-    "arabic",
-    "arabic_lang",
-    "bot",
-    "commands",
-    "english_lang",
-    "export_gban",
-    "giverank",
-    "id",
-    "italian_lang",
-    "moderation",
-    "persian_lang",
-    "plugins",
-    "portuguese_lang",
-    "rules",
-    "settings",
-    "spam",
-    "spamish_lang",
-    "version",
     "quran",
     "map",
     "mananger",
@@ -521,53 +503,6 @@ convert text to image
 *Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
 
 *Only owner can use res,setowner,promote,demote and log commands
-
-Commands list for a super group:
-
-#bot on: enable bot in current channel.
-#bot off: disable bot in current channel.
-#commands: Show all commands for every plugin.
-#commands [plugin]: Commands for that plugin.
-#gbans installer: Return a lua file installer to share gbans and add those in another bot in just one command.
-#gbans list: Return an archive with a list of gbans.
-#install gbans: add a list of gbans into your redis db.
-#rank admin (reply): add admin by reply.
-#rank admin /: add admin by user ID/Username.
-#rank mod (reply): add mod by reply.
-#rank mod /: add mod by user ID/Username.
-#rank guest (reply): remove admin by reply.
-#rank guest /: remove admin by user ID/Username.
-#admins: list of all admin members.
-#mods: list of all mod members.
-#members: list of all channel members.
-id.lua   #id: Return your ID and the chat id if you are in one.
-#ids chat: Return the IDs of the current chat members.
-#ids channel: Return the IDs of the current channel members.
-#id : Return the member username ID from the current chat.
-#whois /: Return username.
-#whois (reply): Return user id.
-#rules: shows chat rules you set before or send default rules.
-#setrules : set chat rules. #remrules: remove chat rules and return to default ones.
-#add: replying to a message, the user will be added to the current group/supergroup.
-#add /: adds a user by its ID/Username to the current group/supergroup.
-#kick: replying to a message, the user will be kicked in the current group/supergroup.
-#kick /: the user will be kicked by its ID/Username in the current group/supergroup.
-#kickme: kick yourself.
-#ban: replying to a message, the user will be kicked and banned in the current group/supergroup.
-#ban /: the user will be banned by its ID/Username in the current group/supergroup and it wont be able to return.
-#unban: replying to a message, the user will be unbanned in the current group/supergroup.
-#unban /: the user will be unbanned by its ID/Username in the current group/supergroup.
-#gban: replying to a message, the user will be kicked and banned from all groups/supergroups.
-#gban /: the user will be banned by its ID/Username from all groups/supergroups and it wont be able to enter.
-#ungban: replying to a message, the user will be unbanned from all groups/supergroups.
-#ungban /: the user will be unbanned by its ID/Username from all groups/supergroups.
-#mute: replying to a message, the user will be silenced in the current supergroup, erasing all its messages.
-#mute /: the user will be silenced by its ID/Username inthe current supergroup, erasing all its messages.
-#unmute: replying to a message, the user will be unsilenced in the current supergroup.
-#unmute /: the user will be unsilenced by its ID/Username in the current supergroup.
-#rem: replying to a message, the message will be removed.
-#settings links enable/disable: when enabled, all links will be cleared.
-#settings arabic enable/disabl: when enabled, all messages with arabic/
 ]]
 }
   serialize_to_file(config, './data/config.lua')
