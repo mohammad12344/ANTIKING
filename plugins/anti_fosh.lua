37d74f20a1f5fd5bb317ae60a395e16c78db7f11
mohammad12344 <sdf12345641@gmail.com>
2016-03-01 16:44:12 +0330

    Update anti_fosh.lua

--- plugins/anti_fosh.lua
+++ plugins/anti_fosh.lua
@@ -1,7 +1,7 @@
 local function run(msg)
     if msg.to.type == 'chat' and not is_momod(msg) then
         chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-        return 'you can not say it!'
+        return '!!چه بی تربیت'
     end
 end
 
