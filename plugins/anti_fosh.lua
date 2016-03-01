69a74b90e024e34dda2e2d70e7c01545661bcb33
mohammad12344 <sdf12345641@gmail.com>
2016-03-01 16:41:08 +0330

    Update anti_fosh.lua

--- plugins/anti_fosh.lua
+++ plugins/anti_fosh.lua
@@ -1,7 +1,7 @@
 local function run(msg)
     if msg.to.type == 'chat' and not is_momod(msg) then
         chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-        return 'استفاده از کلمات بد ممنوع است'
+        return 'you can not say it!'
     end
 end
 
